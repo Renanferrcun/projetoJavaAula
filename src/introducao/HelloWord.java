425577aba51f62df7a36f4e80e608b3040d55f7f
Renan <renanfcunha@gmail.com>
2026-04-16 16:59:42 -0300

    Inicializando Java

---
+++ src/introducao/HelloWord.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hello World");
+	}
+
+}
---
+++ src/introducao/VariaveisConstantes.java
@@ -0,0 +1,30 @@
+package introducao;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		
+		
+		// Variáveis
+		int idade = 25;
+		String nome = "Lilian";
+		float graus = 20.0f;
+		boolean resposta = true;
+		
+		
+		// Constantes
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;
+		
+		System.out.println(idade);
+		System.out.println(nome);
+		System.out.println(graus);
+		System.out.println(resposta);
+		System.out.println(QUANTIDADE);
+		System.out.println(TURMA);
+		System.out.println(AREA);
+		
+	}
+
+}
