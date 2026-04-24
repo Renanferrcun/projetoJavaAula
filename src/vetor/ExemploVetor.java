3164f6c760cb9c8979a9690c9ac84ade61fff465
Renan <renanfcunha@gmail.com>
2026-04-24 14:11:11 -0300

    Vetores e Coleções

---
+++ src/vetor/ExemploVetor.java
@@ -0,0 +1,20 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class ExemploVetor {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		String vetorCachorros[] = { "Boxer", "Pastor A", "Pinscher", "Husky", "Corgi" };
+		
+		for(int numero = 0; numero < 5; numero++) {
+			System.out.println((numero +1) + "º elemento " + vetorCachorros[numero]);
+		}
+		
+
+	}
+
+}
---
+++ src/vetor/ExemploVetor2.java
@@ -0,0 +1,26 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class ExemploVetor2 {
+
+	public static void main(String[] args) {
+
+		int vetorNumeros[] = new int [3];
+		Scanner leia = new Scanner(System.in);
+		
+		for(int indice = 0; indice < 3; indice++) {
+			
+			System.out.println("Digite um número: ");
+			vetorNumeros[indice] = leia.nextInt();
+		}
+		
+		for(int indice = 0; indice < 3; indice++) {
+			System.out.println((indice + 1) + "º elemento: " + vetorNumeros[indice]);
+		}
+		
+		System.out.println("Tamamho do Vetor: " + vetorNumeros.length);
+
+	}
+
+}
